e181e04468c316ff44af8feb2bbc76983b36e094
DumDereDum <46279571+DumDereDum@users.noreply.github.com>
2020-01-28 19:52:32 +0300

    Add files via upload

--- include/Group.h
+++ include/Group.h
@@ -11,7 +11,14 @@ private:
     std::vector<Student*> students;
     Student* head;
 public:
-    Group(/* args */);
+    Group(std::string title, std::string spec);
+    void addStudent(Student* student);
+    void electionOfHead();
+    size_t findByName(std::string name) const;
+    size_t findByID(unsigned int id) const;
+    void estimate(unsigned int id);
+    int findMean() const;
+    void exclude(unsigned int id);
     ~Group();
 };
 
--- include/Student.h
+++ include/Student.h
@@ -7,12 +7,17 @@ class Group;
 class Student
 {
 private:
-    unsigned int ID;
+    unsigned int id;
     std::string fio;
     Group* group;
     std::vector<int> marks;
 public:
-    Student(int id, std::string full_name);
+    Student(int ID, std::string full_name);
+    void enroll(Group* group);
+    void addMark(int mark);
+    int calcMean() const;
+    std::string getFIO() const; 
+    unsigned int getID() const;
     ~Student();
 };
 
