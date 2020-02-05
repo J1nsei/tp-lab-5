f8ba0dd54c4fe6884a9a33b0bffe7be6ecf2cd38
Polyukhov Vladislav <vlad.polyukhov@gmail.com>
2020-02-06 00:40:12 +0300

    Final Project

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,36 +2,21 @@
 #include "../include/dean.h"
 #include "../include/group.h"
 #include "../include/student.h"
-#include <vector>
-#include <string>
-using namespace std;
 
 TEST(Deanery, dean_1) {
-    Dean* dean = new Dean();
-    EXPECT_EQ(4, dean->getGroups());
-    delete dean;
-}
-
-TEST(Deanery, dean_2) {
-    Dean* dean = new Dean();
-    EXPECT_EQ(100, dean->getStudents());
-    delete dean;
-}
-
-TEST(Deanery, dean_3) {
     Group* group = new Group("18-ПМИ");
     EXPECT_EQ("ПМИ", group->getSpec());
     delete group;
 }
 
-TEST(Deanery, dean_4) {
+TEST(Deanery, dean_2) {
     Student* student = new Student("Полюхов Владислав Вадимович", "18-ПМИ");
     student->setMarks();
     EXPECT_EQ(10, student->getMarks().size());
     delete student;
 }
 
-TEST(Deanery, dean_5) {
+TEST(Deanery, dean_3) {
 	Student* student_1 = new Student();
 	Student* student_2 = new Student("Полюхов Владислав Вадимович", "18-ПМИ");
 	EXPECT_EQ(-1, student_1->getId());
@@ -41,7 +26,7 @@ TEST(Deanery, dean_5) {
 	delete student_2;
 }
 
-TEST(Deanery, dean_6) {
+TEST(Deanery, dean_4) {
 	Group* group = new Group("18-ПМИ");
     Student* student = new Student("Полюхов Владислав Вадимович", "18-ПМИ");
 	group->addStudent(*student);
