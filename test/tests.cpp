de51379b787a61b53fc5155bccc5d21eccba93b4
Baturina Anna <baturinaanya@yandex.ru>
2020-02-08 00:14:51 +0300

    first try

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,9 @@
 #include "gtest/gtest.h"
+#include "Deanery.h"
+
+TEST(student, get_fio)
+{
+	Student* stud = Student(0, "FIO of student");
+	string FIO = stud->get_fio();
+	EXPECT_EQ("FIO of student", FIO);
+}
