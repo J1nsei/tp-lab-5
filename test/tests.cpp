3797032506103f2bf225e6638f7d195f4caef8cc
Ilya Tikhobaev <dontixabon@gmail.com>
2020-02-09 17:13:27 +0300

    Fix tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -69,27 +69,5 @@ TEST(task1, case_4) {
     PI_1->addStudent(student_1);
     PI_1->addStudent(student_2);
 
-    PI_1->excludeStudent(student_1)
-
-    EXPECT_EQ(PI_1->getStudents().size(), 1);
-}
-
-
-TEST(task1, case_5) {
-    Group* PI_1 = new Group("18PI-1", "Software engineering");
-    Student* student_1 = new Student(0, "Mikhail Zhelezin");
-    Student* student_2 = new Student(1, "Maria Krylova");
-
-    student_1->addMark(8);
-    student_1->addMark(6);
-    student_2->addMark(8);
-    student_2->addMark(10);
-
-    student_1->addToGroup(PI_1);
-    student_2->addToGroup(PI_1);
-
-    PI_1->addStudent(student_1);
-    PI_1->addStudent(student_2);
-
     EXPECT_EQ(PI_1->findFio("Maria Krylova"), student_2);
 }
