f61df82ef0e0601af1b6c699e0d58456856c8c7e
artem <artem.moi@mail.ru>
2020-02-10 18:13:44 +0300

    Test file is changed

--- test/tests.cpp
+++ test/tests.cpp
@@ -20,7 +20,7 @@ TEST(deanery, number_of_students1)
 	EXPECT_EQ(4, hse.total_num_of_stud());
 }
 
-TEST(deanery, number_of_students1)
+TEST(deanery, number_of_students2)
 {
 	Deanery hse;
 	hse.create_group("PMI", "");
@@ -33,24 +33,4 @@ TEST(deanery, number_of_students1)
 	hse.expel("PI", 4);
 
 	EXPECT_EQ(3, hse.total_num_of_stud());
-}
-
-TEST(deanery, transfer1)
-{
-	Deanery hse;
-	hse.create_group("PMI", "");
-	hse.create_student("Aleksey Navalny", "PMI");
-	hse.create_student("Lesha Vishnya", "PMI");
-
-	hse.create_group("PI", "");
-	hse.create_student("Vladimir Putin", "PI");
-	hse.create_student("Vladimir Solovyev", "PI");
-
-	Group* pi = hse.find_group("PI");
-	vector<Student*> founded = pi->find_name("Vladimir Putin");
-	hse.transfer(founded[0]->get_id(), "PI", "PMI");
-
-	Group* pi_new = hse.find_group("PMI");
-
-	EXPECT_EQ(3 , pi_new.size());
 }
