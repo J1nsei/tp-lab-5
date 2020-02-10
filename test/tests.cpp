1381ab2db1599adb6d26a64d42d4d509094aa4bd
rainbowsock <rainbow_sock@mail.ru>
2020-02-10 18:43:52 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,25 @@
 #include "gtest/gtest.h"
+#include "Deanery.h"
+#include "Student.h"
+#include "Group.h"
+#include <string>
+
+TEST (testGroup, averageMark) 
+{
+	Student student1(1, "Logan Antonio Hughes");
+	Student student2(2, "Cameron Elijah Bennett");
+	Student student3(3, "Wyatt Isaiah Ramirez");
+	Group group("18PI");
+
+	group.addStudent(&student1);
+	group.addStudent(&student2);
+	group.addStudent(&student3);
+
+	for (int i = 0; i < 9; i++) 
+	{
+		student1.setMarks(i);
+		student2.setMarks(i + 1);
+		student3.setMarks(i + 2);
+	}
+	EXPECT_EQ(5, group.averageMarkGroup());
+}
