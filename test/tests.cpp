80c3565097a38481f30743c97eff372f6da40e74
Artyom Pyanzin <aapyanzin@edu.hse.ru>
2020-02-10 20:05:26 +0300

    fixed tests.cpp #2

--- test/tests.cpp
+++ test/tests.cpp
@@ -24,13 +24,17 @@ TEST(testGroup, group_1) {
 	Group* group1 = new Group("AMI");
 	Group* group2 = new Group("SE");
 	Student* student1 = new Student("Oscar Wilde", "AMI");
-	Student* student2 = new Student("Jerome Salinger", "18-AMI");
+	Student* student2 = new Student("Jerome Salinger", "SE");
 	group1->addStudent(student1);
 	group1->addStudent(student2);
 	group1->appointHead();
 	group2->appointHead();
 	EXPECT_EQ("Oscar Wilde", group1->getHeadName());
-	EXPECT_EQ("Jerome Salinger", group1->getHeadName());
+	EXPECT_EQ("Jerome Salinger", group2->getHeadName());
+	delete student1;
+	delete student2;
+	delete group1;
+	delete group2;
 }
 
 TEST(testDean, dean_1) {
@@ -46,4 +50,9 @@ TEST(testDean, dean_1) {
 	dean->setHeads();
 	EXPECT_EQ("Oscar Wilde", dean->getGroups()[0]->getHeadName());
 	EXPECT_EQ("Jerome Salinger", dean->getGroups()[1]->getHeadName());
+	delete student1;
+	delete student2;
+	delete group1;
+	delete group2;
+	delete dean;
 }
