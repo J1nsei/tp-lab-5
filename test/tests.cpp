cbc6aa9ddb8f1277473a905abf1e130bb103d837
Andrei Zhuravlev <dwasdx@gmail.com>
2020-02-11 22:15:16 +0300

    fixed tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -36,7 +36,7 @@ TEST(group_test, add_student_test) {
 TEST(group_test, transfer_test) {
 	Group* gr1 = new Group("18PI");
 	Group* gr2 = new Group("18FM");
-	Student* st = new Student("1 Кузьмин Никита Сергеевич");
+	Student* st = new Student("1 Кузьмин Никита Сергеевич", NULL);
 	gr1->add_student(st);
 	gr1->transfer_to_group(1, gr2);
 	ASSERT_FALSE(gr1->is_student_exist(1));
@@ -45,7 +45,7 @@ TEST(group_test, transfer_test) {
 
 TEST(group_test, expell_test) {
 	Group* gr = new Group("18PI");
-	Student* st = new Student("1 Шабров Илья Дмитриевич");
+	Student* st = new Student("1 Шабров Илья Дмитриевич", NULL);
 	gr->add_student(st);
 	ASSERT_TRUE(gr->is_student_exist(1));
 	gr->expell_by_id(1);
@@ -54,14 +54,14 @@ TEST(group_test, expell_test) {
 
 TEST(group_test, search_test) {
 	Group* gr = new Group("18PI");
-	Student* st1 = new Student("1 A1 A2 A3");
-	Student* st2 = new Student("2 B1 B2 B3");
-	Student* st3 = new Student("3 C1 C2 C3");
+	Student* st1 = new Student("1 A1 A2 A3", NULL);
+	Student* st2 = new Student("2 B1 B2 B3", NULL);
+	Student* st3 = new Student("3 C1 C2 C3", NULL);
 	gr->add_student(st1);
 	gr->add_student(st2);
 	gr->add_student(st3);
 	EXPECT_EQ(st1, gr->search_student(1));
-	EXPECT_EQ(st2, gr->search_student("B1 B2 B3"));
+	EXPECT_EQ(st2, gr->search_student("B1 B2 B3", NULL));
 }
 
 
