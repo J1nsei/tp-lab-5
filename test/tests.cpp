cd443d171bb9bcb6fb173ade34a9f654aa1966c7
AlNualD <nuald@outlook.com>
2020-02-11 13:39:13 +0300

    stupid mistake

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,7 +5,7 @@ TEST(lab5, first_check) {
 	Student* student = new Student(1234, "Nick F");
 	string str = student->getFIO();
 	EXPECT_EQ(str, "Nick F");
-	EXPECT_EQ(234, student->getID);
+	EXPECT_EQ(234, student->getID());
 }
 
 
