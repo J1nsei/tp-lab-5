0c6873d7d728d74273b8f0dcbc872e58d229afa5
AlNualD <nuald@outlook.com>
2020-02-11 12:48:37 +0300

    add test1

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,13 @@
 #include "gtest/gtest.h"
+#include "deanery.h"
+
+TEST(lab5, first_check) {
+	Student* student = new Student(1234, "Nick F");
+	string str = student->getFIO();
+	EXPECT_EQ(str, "Nick F");
+	EXPECT_EQ(234, student->getID);
+}
+
+
+
+
