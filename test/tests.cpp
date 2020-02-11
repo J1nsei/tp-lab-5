8417a5a92ff933c9604080a765c6c50910a44f4a
unknown <nastenckavoronowa@yandex.ru>
2020-02-11 08:51:05 +0300

    seven

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,10 +7,10 @@ TEST(testStudent, middle_mark)
 {
 	Student* student = new Student(10,"Alex Petrov", "1");
 	
-		student.AddMark(1);
-		student.AddMark(2);
-		student.AddMark(3);
-		student.AddMark(4);
+		student->AddMark(1);
+		student->AddMark(2);
+		student->AddMark(3);
+		student->AddMark(4);
 	
 	EXPECT_EQ(2.5, student->MiddleMark());
 	
@@ -22,16 +22,16 @@ TEST(testGroup, middle_mark)
 	Student* student1 = new Student(10, "Alex Petrov", "1");
 	Student* student2 = new Student(11, "Alex Karpov", "1");
 
-	student1.AddMark(1);
-	student1.AddMark(2);
-	student1.AddMark(3);
-	student1.AddMark(4);
-	student2.AddMark(1);
-	student3.AddMark(2);
-	student4.AddMark(3);
-	student5.AddMark(4);
-	group1.AddStudent(stedunt1);
-	group1.AddStudent(stedunt2);
+	student1->AddMark(1);
+	student1->AddMark(2);
+	student1->AddMark(3);
+	student1->AddMark(4);
+	student2->AddMark(1);
+	student3->AddMark(2);
+	student4->AddMark(3);
+	student5->AddMark(4);
+	group1->AddStudent(student1);
+	group1->AddStudent(student2);
 
 	EXPECT_EQ(2.5, group1->MiddleGroupMark());
 
