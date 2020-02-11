2b45bac689d353872cb38ed04fcf899bb1cc2361
smvolokh <sema.volokh@yandex.ru>
2020-02-11 17:21:04 +0300

    add tests_2

--- test/tests.cpp
+++ test/tests.cpp
@@ -52,3 +52,33 @@ TEST(tplab5, test2)
 	Student stud_for_check = pmi18_1->search_student_name("Петров Пётр Евгеньевич");
 	EXPECT_NE(stud_for_check.getId(), 0);
 }
+
+TEST(tplab5, test3)
+{
+	setlocale(LC_ALL, "Russian");
+	Dean imikn("imikn");
+	imikn.create_group("18ПМИ-1", "Программирование");
+	imikn.create_group("18ПИ-1", "Инженерия");
+	imikn.add_student_to_group("Воронов Пётр Алексеевич", "18ПМИ-1");
+	imikn.add_student_to_group("Петров Пётр Евгеньевич", "18ПИ-1");
+	imikn.add_student_to_group("Яковлев Андрей Михайлович", "18ПИ-1");
+	imikn.add_student_to_group("Шубин Константин Алексеевич", "18ПМИ-1");
+
+	EXPECT_EQ(imikn.search_group_name("18ПИ-1")->get_title(), "18ПИ-1");
+}
+
+TEST(tplab5, test4)
+{
+	setlocale(LC_ALL, "Russian");
+	Dean imikn("imikn");
+	imikn.create_group("18ПМИ-1", "Программирование");
+	imikn.create_group("18ПИ-1", "Инженерия");
+	imikn.add_student_to_group("Воронов Пётр Алексеевич", "18ПМИ-1");
+	imikn.add_student_to_group("Петров Пётр Евгеньевич", "18ПИ-1");
+	imikn.add_student_to_group("Яковлев Андрей Михайлович", "18ПИ-1");
+	imikn.add_student_to_group("Шубин Константин Алексеевич", "18ПМИ-1");
+	Group* pi18_1 = imikn.search_group_name("18ПИ-1");
+	for (int i = 1; i < 10; i++)
+		imikn.add_mark_to_student("18ПИ-1", "Яковлев Андрей Михайлович", i);
+	EXPECT_EQ(pi18_1->search_student_name("Яковлев Андрей Михайлович").avg_mark(), 5);
+}
