cf7ff68578820afe6802037889581eda37088f63
Dasha <wqe>
2020-02-12 10:07:17 +0300

    fix3

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,8 +10,8 @@ delete group;
 
 TEST(lab5, task2){
 Student* student = new Student("Ювелева Екатерина Леонидовна");
-student->addMark(4);
-student->addMark(6);
+student->AddMark(4);
+student->AddMark(6);
 EXPECT_EQ(5,student->GetAverageMark());
 delete student;
 }
@@ -26,10 +26,10 @@ TEST(lab5, task4){
 Group* group = new Group("19БИ-2");
 Student* s1 = new Student("Ювелева Екатерина Леонидовна");
 Student* s2 = new Student("Астанков Петр Аникитевич");
-group->addStudent(s1);
-group->addStudent(s2);
-s1->addMark(9);
-s2->addMark(7);
+group->AddStudent(s1);
+group->AddStudent(s2);
+s1->AddMark(9);
+s2->AddMark(7);
 EXPECT_EQ(8,group->GetAverageGroupMark());
 delete group;
 }
