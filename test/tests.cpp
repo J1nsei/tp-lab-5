75fc348a899efff7b35fe5418d42f8612ed7e3ba
Anzorenus <Anzorenus@yandex.ru>
2020-06-14 22:36:33 +0300

    Ogurliev 18PMI-1

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,11 @@
 #include "gtest/gtest.h"
+#include "student.h"
+#include <string>
+
+TEST(testStudent, init)
+{
+	Student* s = new Student(5, "Vasya Petrov");
+	int id;
+	id = s->get_id();
+	EXPECT_EQ(5, id);
+}
